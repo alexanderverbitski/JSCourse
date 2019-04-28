6e2993db086ddf355928c900d79f0fbcf9430761
Alexander Verbitski <alexanderverbitski@Alexanders-MacBook-Pro-2.local>
2019-04-28 11:47:14 +0300

    "Log output for action()"

--- utils/wait.util.js
+++ utils/wait.util.js
@@ -16,17 +16,21 @@ class Wait {
   forTrue(action, maxCount, interval, expectedValue, count = 0) {
     count++;
     logger.info(`[${count}] Wait for true`);
-    return doWait(action, interval, expectedValue).then(() => {
-      logger.info('Was able to reach expected condition!');
-      return true;
-    }, () => {
-      if (maxCount <= count) {
-        logger.warning('Was not able to reach expected condition!');
-        return false;
-      } else {
-        return this.forTrue(action, maxCount, interval, expectedValue, count);
-      }
-    })
+    return doWait(action, interval, expectedValue)
+        .then(
+        () => {
+            logger.info('Was able to reach expected condition!');
+            return true;
+        }, 
+        () => {
+            if (maxCount <= count) {
+            logger.warning('Was not able to reach expected condition!');
+            logger.warning(`[${count}] action() output: ${action()}`);
+            return false;
+        } else {
+            return this.forTrue(action, maxCount, interval, expectedValue, count);
+        }
+    });
   }
 }
 
