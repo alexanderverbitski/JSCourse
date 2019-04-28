95efa6511f25e424d071acb91a69f360f988cf4f
Alexander Verbitski <alexanderverbitski@Alexanders-MacBook-Pro-2.local>
2019-04-28 12:27:00 +0300

    "#3: forFalse() added, #6: tests added"

--- utils/wait.util.js
+++ utils/wait.util.js
@@ -16,6 +16,7 @@ class Wait {
   forTrue(action, maxCount, interval, expectedValue, count = 0) {
     count++;
     logger.info(`[${count}] Wait for true`);
+
     return doWait(action, interval, expectedValue)
         .then(
         () => {
@@ -30,6 +31,27 @@ class Wait {
             return this.forTrue(action, maxCount, interval, expectedValue, count);
         }
     });
+  };
+
+  forFalse(action, maxCount, interval, expectedValue, count = 0){
+      count++;
+      logger.info(`[${count}] Wait for false`);
+
+      return doWait(action, interval, expectedValue)
+      .then(
+          () => {
+            logger.info('Was able to reach expected condition!');
+            return true;
+          },
+          () => {
+            if (maxCount <= count) {
+                logger.warning(`Was not able to reach expected condition! action() output: ${action()}`);
+                return false;
+            } else {
+                return this.forFalse(action, maxCount, interval, expectedValue, count);
+            }
+          }
+      );
   }
 }
 
